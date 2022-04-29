399cb3786e2e9119c6c61eb17d88ea150e36e974
sbhaskara <sb185532@ncr.com>
2022-04-29 11:57:34 -0500

    Initial commit

--- src/main/java/com/example/springfeatures/SpringFeaturesApplication.java
+++ src/main/java/com/example/springfeatures/SpringFeaturesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringfeaturesApplication {
+public class SpringFeaturesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringfeaturesApplication.class, args);
+		SpringApplication.run(SpringFeaturesApplication.class, args);
 	}
 
 }
---
+++ src/test/java/com/example/springfeatures/SpringFeaturesApplicationTests.java
@@ -0,0 +1,4 @@
+package com.example.springfeatures;
+class SpringfeaturesApplicationTests {
+
+}
